42ccc56ea94f30402168831b82e96113dbd13499
Walmyr Filho <limaesilva@qelp.com>
2020-05-12 13:34:12 +0200

    Adjust indentation and remove some trailing spaces
    From destination controller.

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,23 +1,23 @@
 class DestinationsController < ApplicationController
-	def show
+  def show
     @destination = Destination.find(params[:id])
   end
 
-  def edit 
-    @destination = Destination.find(params[:id]) 
+  def edit
+    @destination = Destination.find(params[:id])
   end
 
-  def update 
-  	@destination = Destination.find(params[:id]) 
-  	if @destination.update_attributes(destination_params) 
-      redirect_to(:action => 'show', :id => @destination.id) 
-    else 
-    	render 'edit' 
-  	end 
+  def update
+    @destination = Destination.find(params[:id])
+    if @destination.update_attributes(destination_params)
+      redirect_to(:action => 'show', :id => @destination.id)
+    else
+      render 'edit'
+    end
   end
 
-  private 
-  def destination_params 
-    params.require(:destination).permit(:name, :description) 
+  private
+  def destination_params
+    params.require(:destination).permit(:name, :description)
   end
 end
