2b5888185dc985d1e734746d380daf6c60ee207f
Walmyr Filho <limaesilva@qelp.com>
2020-05-12 02:33:06 +0200

    Make slash the home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/' => 'tags#index'
   get '/tags' => 'tags#index'
   get '/tags/:id' => 'tags#show', as: :tag
   get '/destinations/:id' => 'destinations#show', as: :destination
