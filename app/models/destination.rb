6233f71184f921ceca92b8a26b1be9822288897e
Walmyr Filho <limaesilva@qelp.com>
2020-05-12 13:32:10 +0200

    Add some rules on destination fields
    Now both name and description are mandatory, and they also should
    have a minimum length, of 3 and 10 respectively.

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,3 +1,5 @@
 class Destination < ActiveRecord::Base
 	belongs_to :tag
+	validates :name, :presence => true, length: { minimum: 3 }
+	validates :description, :presence => true, length: { minimum: 10 }
 end
